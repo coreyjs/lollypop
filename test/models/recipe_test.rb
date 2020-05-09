ff997aafca2fdb1a9577e319d6a7c8badffd6893
Corey Schaf <cschaf@gmail.com>
2020-05-09 12:35:49 -0400

    resolves #4 - Adds tests for recipes and tags

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -11,4 +11,27 @@ class RecipeTest < ActiveSupport::TestCase
 
     assert_not recipe.valid?
   end
+
+  test "a recipe can have many tags" do
+    recipe = recipes(:pizza)
+    assert_equal 2, recipe.tags.length
+    tag = Tag.new(name: 'Test Tag')
+    recipe.tags << tag
+
+    assert_equal 3, recipe.tags.length
+  end
+
+  test "recipes can share the same tags" do
+    recipe_1 = recipes(:pizza)
+    recipe_2 = recipes(:soup)
+
+    tag = tags(:kid_friendly)
+
+    recipe_1.tags << tag
+    recipe_2.tags << tag
+
+    assert_includes recipe_1.tags, tag
+    assert_includes recipe_2.tags, tag
+  end
+
 end
