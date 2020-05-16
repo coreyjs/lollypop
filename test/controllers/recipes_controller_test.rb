8de8fea01fd085fc3ba6c9c1ae7426745a3a4f61
Corey Schaf <cschaf@gmail.com>
2020-05-16 17:31:16 -0400

    fixes tests, allows for devise auth in minitest.  resolved #12

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,51 +1,70 @@
 require 'test_helper'
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @recipe = recipes(:pizza)
+    sign_in users(:kaladin)
   end
 
   test "should get index" do
+    sign_out :user
     get recipes_url
     assert_response :success
   end
 
-  test "should get new" do
+  test "should get new if authenticated" do
     get new_recipe_url
     assert_response :success
   end
 
-  test "should create recipe" do
+  test "should create recipe if authenticated" do
+    sign_in users(:kaladin)
+
     assert_difference('Recipe.count') do
-      post recipes_url, params: { recipe: { active: @recipe.active, deleted: @recipe.deleted, description: @recipe.description, name: @recipe.name, slug: 'test' } }
+      post recipes_url, params: { recipe: { active: @recipe.active, deleted: @recipe.deleted,
+                                            description: @recipe.description,
+                                            name: @recipe.name, slug: 'test', prep_time:10, cook_time: 15 } }
     end
 
     assert_redirected_to recipe_url(Recipe.last)
   end
 
   test "should show recipe" do
+    sign_out :user
     get recipe_url(@recipe)
     assert_response :success
   end
 
-  # test "should get edit" do
-  #   get edit_recipe_url(@recipe)
-  #   assert_response :success
-  # end
-  #
-  # test "should update recipe" do
-  #   patch recipe_url(@recipe), params: { recipe: { active: false, deleted: @recipe.deleted, description: @recipe.description, name: @recipe.name, slug: @recipe.slug } }
-  #   assert_redirected_to recipe_url(@recipe)
-  # end
-  #
-  # test "should destroy recipe" do
-  #   assert_difference('Recipe.count', -1) do
-  #     delete recipe_url(@recipe)
-  #   end
-  #
-  #   assert_redirected_to recipes_url
-  # end
-  #
+  test "should get redirected if not logged in on edit" do
+    sign_out :user
+    get edit_recipe_url(@recipe)
+    assert_response :redirect
+  end
+
+  test "should get edit if authenticated" do
+    get edit_recipe_url(@recipe)
+    assert_response :success
+  end
+
+  test "should update recipe if authenticated" do
+    patch recipe_url(@recipe), params: { recipe: { active: false, deleted: @recipe.deleted, description: @recipe.description, name: @recipe.name, slug: @recipe.slug } }
+    assert_redirected_to recipe_url(@recipe)
+  end
+
+  test "should destroy recipe" do
+    assert_difference('Recipe.count', -1) do
+      delete recipe_url(@recipe)
+    end
+
+    assert_redirected_to recipes_url
+   end
+
+  test "cant edit someone elses recipe" do
+
+  end
+
   test "a recipe has a NULL rating without a rating" do
 
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,7 +28,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "fixture is valid" do
+    user = users(:kaladin)
+    assert user.valid?
+  end
 end
